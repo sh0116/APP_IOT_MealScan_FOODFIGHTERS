c3658286bff547fb456e917ef8dbde4004e2a53b
seanpark98 <seanpark98@gmail.com>
2021-09-27 03:20:27 +0000

    testing run

--- osam2021_fin/lib/components/login_text_form_field.dart
+++ osam2021_fin/lib/components/login_text_form_field.dart
@@ -1,31 +1,25 @@
-import 'package:flutter/material.dart' show BorderRadius, BuildContext, Column, CrossAxisAlignment, InputDecoration, OutlineInputBorder, SizedBox, StatelessWidget, Text, TextFormField, Widget;
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_login/theme.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_login/size.dart';
 
-class CustomTextFormField extends StatefulWidget {
+class CustomTextFormField extends StatelessWidget {
   final String text;
 
   const CustomTextFormField(this.text);
 
   @override
-  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
-}
-
-class _CustomTextFormFieldState extends State<CustomTextFormField> {
-  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(widget.text),
-        SizedBox(height: 100),
+        Text(text),
+        SizedBox(height: 60),
         TextFormField(
           validator: (value) => value!.isEmpty ? "Please enter some text" : null, // 1. 값이 없으면 Please enter some text 경고 화면 표시
           obscureText:
               // 2. 해당 TextFormField가 비밀번호 입력 양식이면 **** 처리 해주기
-              widget.text == "Password" ? true : false,
+              text == "Password" ? true : false,
           decoration: InputDecoration(
-            hintText: "Enter ${widget.text}",
+            hintText: "Enter $text",
             enabledBorder: OutlineInputBorder(
               // 3. 기본 TextFormField 디자인
               borderRadius: BorderRadius.circular(20),
