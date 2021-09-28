df4717ab6cdfe790b3a7e8d521d3dfa72a646f38
seanpark98 <seanpark98@gmail.com>
2021-09-28 03:32:38 +0000

    set up virtual env and imported modules for python

--- osam2021_flutlab/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF000000);
-const kSecondaryColor = Color(0xFFFFFFFF);
--- osam2021_raspi/database.py
+++ osam2021_raspi/database.py
@@ -1,31 +1,30 @@
 from firebase import firebase
 import datetime
 import time
-import serial
 import sys
 
 # Create the connection to our Firebase database - don't forget to change the URL!
 FBConn = firebase.FirebaseApplication('https://military-cafeteria-default-rtdb.firebaseio.com/', None)
 
 def test():
-    while True:
-        test_id = "20-71209910"
-        test_food1 = '김치'
-        test_food1_amount = 36.7
-        # Create a dictionary to store the data before sending to the database
-        data_to_upload = {
-            'ID' : test_id,
-            'Food1': test_food1,
-            'Food1_Amount': test_food1_amount
-        }
+    test_id = "20-71209910"
+    test_food1 = '김치'
+    test_food1_amount = 36.7
+    print(True)
+    # Create a dictionary to store the data before sending to the database
+    data_to_upload = {
+        'ID' : test_id,
+        'Food1': test_food1,
+        'Food1_Amount': test_food1_amount
+    }
 
-        # Post the data to the appropriate folder/branch within your database
-        result = FBConn.post(test_id,data_to_upload)
+    # Post the data to the appropriate folder/branch within your database
+    result = FBConn.post(test_id,data_to_upload)
 
 
-        # Print the returned unique identifier
-        print(result)
-        sys.exit(0)
+    # Print the returned unique identifier
+    print(result)
+    exit(0)
 
     # Close the serial connection
     #ser.close()
