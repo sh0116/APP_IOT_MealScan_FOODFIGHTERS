3d4ab3ba3e3b0c33c5faed49da797c9bc7cac4c1
Sean Lee <slee1040@stanford.edu>
2021-09-28 10:24:40 +0000

    merging frontend to master

---
+++ osam2021_raspi/database.py
@@ -0,0 +1,29 @@
+from firebase import firebase
+import datetime
+import time
+import sys
+
+# Create the connection to our Firebase database - don't forget to change the URL!
+FBConn = firebase.FirebaseApplication('https://military-cafeteria-default-rtdb.firebaseio.com/', None)
+def test():
+    test_id = "20-71209876"
+    test_food1 = '김치'
+    test_food1_amount = 36.7
+    # Create a dictionary to store the data before sending to the database
+    data_to_upload = {
+        'ID' : test_id,
+        'Food1': test_food1,
+        'Food1_Amount': test_food1_amount
+    }
+
+    # Post the data to the appropriate folder/branch within your database
+    result = FBConn.post(test_id,data_to_upload)
+
+
+    # Print the returned unique identifier
+    print(result)
+    return result
+test()
+    #exit(0)
+    # Close the serial connection
+    #ser.close()
---
+++ osam2021_raspi/utill.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import glob
+def processLog(filename):
+    images = cv2.imread(filename);
+    # Open this image and make a Numpy version for easy processing
+    imnp = np.array(images)
+    h, w = imnp.shape[:2]
+    # Get list of unique colours...
+    # Arrange all pixels into a tall column of 3 RGB values and find unique rows (colours)
+    colours, counts = np.unique(imnp.reshape(-1,3), axis=0, return_counts=1)
+    # Iterate through unique colours
+    count = counts[0]
+    proportion = (100 * count) / (h * w)
+    colour = [0,0,0]
+    print(f"   Colour: {colour}, count: {count}, proportion: {proportion:.2f}%")
+# Iterate over all images called "log*png" in current directory
+def main():
+    for filename in ["result2.png","result3.png","result5.png","result6.png","result4.png"]:
+        processLog(filename)
