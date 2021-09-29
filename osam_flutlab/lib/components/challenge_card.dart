b54278f8d9b8bdf170604fd28600fd72649ccfd6
Sean Lee <slee1040@stanford.edu>
2021-09-29 20:17:01 +0900

    Update challenge_card.dart
    card revamp

--- osam_flutlab/lib/components/challenge_card.dart
+++ osam_flutlab/lib/components/challenge_card.dart
@@ -8,6 +8,7 @@ import 'package:slide_countdown/slide_countdown.dart';
 import 'package:osam2021/notifiers.dart';
 import 'package:provider/provider.dart';
 
+
 class ChallengeCard extends StatefulWidget {
   final Function() notifyParent;
   final Challenge challenge;
@@ -17,7 +18,8 @@ class ChallengeCard extends StatefulWidget {
   @override
   _ChallengeCardState createState() => _ChallengeCardState();
 }
-//https://pub.dev/packages/flip_card
+//https://pub.dev/packages/buttons_tabbar
+//https://pub.dev/packages/animated_button_bar
 //https://sergiandreplace.com/planets-flutter-creating-a-planet-card/
 //https://flutterawesome.com/a-flutter-widget-with-the-goal-of-simplifying-styling-and-to-reduce-nesting/
 class _ChallengeCardState extends State<ChallengeCard> {
@@ -33,32 +35,32 @@ class _ChallengeCardState extends State<ChallengeCard> {
     const defaultPadding = EdgeInsets.symmetric(horizontal: 10, vertical: 3);
     return Column(
       children: [
-        // Padding(
-        //   padding: const EdgeInsets.only(left: 8.0),
-        //   child: Row(
-        //     mainAxisAlignment: MainAxisAlignment.start,
-        //     children: [
-        //       SlideCountdown(
-        //         duration: defaultDuration,
-        //         padding: defaultPadding,
-        //         slideDirection: SlideDirection.up,
-        //         fade: true,
-        //         decoration: BoxDecoration(
-        //           color: Colors.black,
-        //           borderRadius: BorderRadius.all(Radius.circular(5)),
-        //         ),
-        //         icon: Padding(
-        //           padding: EdgeInsets.only(right: 5),
-        //           child: Icon(
-        //             Icons.alarm,
-        //             color: Colors.white,
-        //             size: 15,
-        //           ),
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SlideCountdown(
+              duration: defaultDuration,
+              padding: defaultPadding,
+              slideDirection: SlideDirection.up,
+              fade: true,
+              decoration: BoxDecoration(
+              color: widget.challenge.bgColor,
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+                 ),
+                 icon: Padding(
+                   padding: EdgeInsets.only(right: 5),
+                   child: Icon(
+                     Icons.alarm,
+                     color: Colors.white,
+                     size: 15,
+                   ),
+                 ),
+               ),
+            ],
+          ),
+        ),
         // Card(
         //     child: InkWell(
         //   onTap: () {
@@ -75,73 +77,153 @@ class _ChallengeCardState extends State<ChallengeCard> {
         //     isThreeLine: true,
         //   ),
         // )),
-      _createCard(),
+       //_openCard(),
+      // _createCard(),
+
+      _selectCard(),
         
         SizedBox(height: 30)
       ],
     );
   }
 
-  Widget _createCard() {
-    return Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 220,
-                  child: Container(
-                      padding: const EdgeInsets.only(left: 20, top: 25, right: 20),
-                      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                        Text("Next workout",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                            )),
-                        SizedBox(height: 5),
-                        Text("Legs Toning",
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.white,
-                            )),
-                        SizedBox(height: 5),
-                        Text("and Glutes Workout",
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.white,
-                            )),
-                        SizedBox(height: 25),
-                        Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                          Row(children: [
-                            Icon(Icons.timer, size: 20, color: Colors.white),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text("60 min",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.white,
-                                )),
-                          ]),
-                          Expanded(child: Container()),
-                          Container(
-                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(60), boxShadow: [
-                                BoxShadow(color: Color(0xff0f17ad), blurRadius: 10, offset: Offset(4, 8))
-                              ]),
-                              child: Icon(Icons.play_circle_fill, color: Colors.white, size: 60))
-                        ]),
-                      ])),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                        Color(0xff0f17ad).withOpacity(0.8),
-                        Color(0xFF6985e8).withOpacity(0.9),
-                      ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                        topRight: Radius.circular(80),
-                      ),
-                      boxShadow: [
-                        BoxShadow(offset: Offset(5, 10), blurRadius: 20, color: Color(0xFF6985e8).withOpacity(0.2))
-                      ]));
+  Widget _selectCard() {
+    return widget.added ? _addedCard() : _openCard();
   }
+
+  Widget _addedCard() {
+        DateFormat format = DateFormat("hh:mm:ss");
+    int estimateTs = DateTime.parse(widget.challenge.date).millisecondsSinceEpoch;
+    int now = DateTime.now().millisecondsSinceEpoch;
+    Duration remaining = Duration(milliseconds: estimateTs - now);
+    String formattedRemaining = '${remaining.inDays}:${format.format(DateTime.fromMillisecondsSinceEpoch(remaining.inMilliseconds))}';
+    var date = formattedRemaining.split(":");
+    Duration defaultDuration = Duration(days: remaining.inDays, hours: int.parse(date[1]), minutes: int.parse(date[2]));
+    const defaultPadding = EdgeInsets.symmetric(horizontal: 10, vertical: 3);
+        String name = widget.challenge.name;
+    return Container(
+      child: InkWell(
+        onTap: () {
+          _navigateToAddedInfoScreen(context);
+        },
+        child: Container(
+            padding: const EdgeInsets.only(left: 20, top: 25, right: 20),
+            decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          widget.challenge.bgColor.withOpacity(0.8),
+                          widget.challenge.bgColor2.withOpacity(0.9),
+                        ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                          topRight: Radius.circular(55),
+                        ),
+                        boxShadow: [
+                          BoxShadow(offset: Offset(5, 10), blurRadius: 20, color: widget.challenge.bgColor2.withOpacity(0.2))
+                        ]),
+          width: MediaQuery.of(context).size.width,
+          height: 90,
+          child: 
+          Column(
+            children: [
+              Row(children:
+               [Text("$name", style: TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold)),
+                Icon(FontAwesomeIcons.chevronRight, color: Color(0xff999999), size: 30),
+               ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               ),
+            ],
+          ),
+
+        ),
+      ),
+    );   
+  }
+
+  Widget _openCard() {
+    String name = widget.challenge.name;
+    String attendant = widget.challenge.attendants + "명 참가 중";
+
+    var dest = DateTime.parse(widget.challenge.date);
+    var today = DateTime.now();
+    var from = DateTime(dest.year, dest.month, dest.day);
+    var to = DateTime(today.year, today.month, today.day);
+    int days = (from.difference(to).inHours / 24).round();
+    String date = '$days' + "일 후 마감";
+    return Container(
+      child: InkWell(
+        onTap: () {
+          if (widget.added) {
+              _navigateToAddedInfoScreen(context);
+          } else {
+              _navigateToChallengeInfo(context);
+          }
+        },
+        child: Container(
+                    padding: const EdgeInsets.only(left: 20, top: 25, right: 20),
+                  
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          widget.challenge.bgColor.withOpacity(0.8),
+                          widget.challenge.bgColor2.withOpacity(0.9),
+                        ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                          topRight: Radius.circular(80),
+                        ),
+                        boxShadow: [
+                          BoxShadow(offset: Offset(5, 10), blurRadius: 20, color: widget.challenge.bgColor2.withOpacity(0.2))
+                        ]),
+          width: MediaQuery.of(context).size.width,
+          height: 130,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(children: [Text("$name", style: TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold)),]),
+              SizedBox(height: 3),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 4),
+                      Icon(Icons.hourglass_bottom, size: 16, color: Colors.black),
+                      SizedBox(
+                         width: 10,
+                      ),
+                      Text("$date",
+                          style: TextStyle(fontSize: 14, color: Colors.black,)
+                      ),                    
+                    ]
+                  ),
+                  Expanded(child: Container()),
+                  Icon(FontAwesomeIcons.chevronRight, color: Color(0xff999999), size: 30),
+                ]
+              ),
+              SizedBox(height:3),
+               Row(
+                    children: [
+                      SizedBox(width: 4),
+                      Icon(FontAwesomeIcons.users, size: 16, color: Colors.black),
+                      SizedBox(
+                         width: 10,
+                      ),
+                      Text("$attendant",
+                          style: TextStyle(fontSize: 14, color: Colors.black,)
+                      ),                    
+                    ]
+                  ),
+            ]
+          ),
+        ),
+      ),
+    );
+
+  }
+
 
   Widget _buildCardSubtitles(Duration duration, var padding) {
     return Row(children: [
@@ -190,7 +272,7 @@ class _ChallengeCardState extends State<ChallengeCard> {
     widget.notifyParent();
   }
 
-  _navigateToInfoScreen(BuildContext context) async {
+  _navigateToAddedInfoScreen(BuildContext context) async {
     await Navigator.of(context).push(MaterialPageRoute<void>(builder: (BuildContext context) {
       return AddedChallengeInfo(challenge: widget.challenge);
     }));
