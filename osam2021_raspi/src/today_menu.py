b42d2ca7b52e52f97f6331c67ec2ef5d1c789222
seanpark98 <seanpark98@gmail.com>
2021-10-10 05:49:32 +0000

    completed implementation of today_menu

--- osam2021_raspi/src/today_menu.py
+++ osam2021_raspi/src/today_menu.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+from datetime import date
 
 excel_file = '제3389부대'
 path = '/workspaces/APP_IOT_AI_Meal-Mil-Scan_FOODFIGHTERS/osam2021_raspi/asset/monthly_menu_base/'+ excel_file +' 식단 정보_월별.csv'
 
 df = pd.read_csv(path, encoding='euc-kr')
-
-print(df['날짜'][0])
+today = date.today()
+print(df.loc[df['날짜'] == today])
