bc6e7cf4be6c609f0e3a8545cac799612d999991
sh0116 <38518675+sh0116@users.noreply.github.com>
2021-10-12 23:46:23 +0900

    Update main.py

--- osam2021_raspi/main.py
+++ osam2021_raspi/main.py
@@ -1,53 +1,54 @@
 import cv2
 import numpy as np
 import time
+
 from src import image_processing
 from src import init_processing
 from src import qr_processing
 from src import database
 
-
+# main process
 class main_process():
     def __init__(self):
        
-        a, b, c = None, None, None
         self.cap = cv2.VideoCapture(0)
+        # cv2.show size
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
-        # self.button dimensions (y1,y2,x1,x2)
+        # button point x1,y1,x2,y2
         self.button = [20,60,50,250]
-        # function that handles the mousclicks
-        #state : init, qr, plate
+        # fist state
         self.state = "init"
+        # open camera
         self.webcam()
 
-
+    # state check function
     def process_click(self, event, x, y, flags, params):
-        # check if the click is within the dimensions of the self.button
         if event == cv2.EVENT_LBUTTONDOWN:
+            # if click botton
             if y > self.button[0] and y < self.button[1] and x > self.button[2] and x < self.button[3]: 
                 print('Clicked on Button!')
+                # check image capture
                 ret, a = self.cap.read()
+
+                # state init 
                 if self.state=="init":
                     init_processing.Image_Processing(a)
                     self.state = "qr"
-                    
+
+                # state qr
                 elif self.state=="qr":
                     self.qr = qr_processing.Image_Processing(a)
                     self.qr_data = self.qr.Data()
+
                     if self.qr_data != "empty data":
                         self.state = "plate"
 
+                # state plate 
                 elif self.state=="plate":
                     process_class = image_processing.Image_Processing(a)
                     result = database.firebase_post(self.qr_data, process_class.DataList )
-                    '''
-                    if result == "failed":
-                        print("retry")
-                    else:
-                        print("ok")
-                        self.state = "qr"
-                    '''
+
                     self.state = "qr"
             
     def webcam(self):
@@ -58,17 +59,21 @@ class main_process():
         control_image = np.zeros((80,300), np.uint8)
         control_image[self.button[0]:self.button[1],self.button[2]:self.button[3]] = 180
         cv2.putText(control_image, 'Click',(100,50),cv2.FONT_HERSHEY_PLAIN, 2,(0),3)
-        #show 'control panel'
+        # show 'control panel'
         cv2.imshow('Control', control_image)
+
+        # open camera
         if self.cap.isOpened():
             ret, a = self.cap.read()
             ret, b = self.cap.read()
-
+            # camera still open
             while ret:
                 ret, c = self.cap.read()
                 draw = c.copy()
+                # close camera
                 if not ret:
                     break
+                # cv2.show() in rectangle() show plate area
                 if self.state!="qr":
                     draw = cv2.rectangle(draw, (50, 50), (430, 270), (0, 255, 0), 2)
                     draw = cv2.rectangle(draw, (245, 155), (420, 260), (0, 255, 0), 2)
@@ -77,18 +82,20 @@ class main_process():
                     draw = cv2.rectangle(draw, (60, 60), (175, 145), (0, 255, 0), 2)
                     draw = cv2.rectangle(draw, (185, 60), (300, 145), (0, 255, 0), 2)
                     draw = cv2.rectangle(draw, (310, 60), (420, 145), (0, 255, 0), 2)
-                    #stacked = np.hstack((draw, cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)))
+
                     if self.state!="init":
                         cv2.imshow('init process', draw)
                     else:
                         cv2.imshow('plate', draw)
+
+                # cv2.show() in rectangle() show qr area
                 else:
                     draw = cv2.rectangle(draw, (190 , 110 ), (290, 210), (0, 255, 0), 2)
-                    #stacked = np.hstack((draw, cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)))
                     cv2.imshow('qr', draw)
+
                 a = b
                 b = c
-
+        # close window
         cv2.destroyAllWindows()
 
 
