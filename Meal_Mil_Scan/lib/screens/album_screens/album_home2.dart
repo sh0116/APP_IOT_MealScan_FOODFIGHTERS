24339dd9933fa61322ef2c2600968530bd6d3021
seanpark98 <seanpark98@gmail.com>
2021-10-14 11:58:45 +0000

    fixed database.py to add image path to firestore

---
+++ Meal_Mil_Scan/lib/screens/album_screens/album_home2.dart
@@ -0,0 +1,188 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'album_details.dart';
+import 'package:intl/intl.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+//final ref =
+//  FirebaseStorage.instance.ref().child('20-71209928/21-10-13-2.png');
+// no need of the file extension, the name will do fine.
+//var url = ref.getDownloadURL();
+//var m = url.toString();
+//StorageReference ref =
+//    FirebaseStorage.instance.ref().child('20-71209928/21-10-13-2.png');
+//String url = (ref.getDownloadURL()).toString();
+
+//Future<String> getFileData() async {
+//    final ref = FirebaseStorage.instance.ref().child('20-71209928/21-10-13-2.png');
+//    var url = await ref.getDownloadURL();
+//    return url.toString();
+//  }
+
+//final m = getFileData();
+List<ImageDetails> _images = [
+  ImageDetails(
+    imagePath: 'assets/images/meal1.jpg',
+    percentage: '80%',
+    mealType: '조식',
+    date: '2021-11-29',
+    details: '',
+  ),
+  ImageDetails(
+    imagePath: 'assets/images/meal2.jpg',
+    percentage: '65%',
+    mealType: '중식',
+    date: '2021-11-30',
+    details: '',
+  ),
+];
+
+class AlbumHome2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _images.sort((a, b) => a.date.compareTo(b.date));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return RawMaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AlbumDetails(
+                            imagePath: _images[index].imagePath,
+                            date: _images[index].date,
+                            mealType: _images[index].mealType,
+                            percentage: _images[index].percentage,
+                            details: _images[index].details,
+                            index: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Hero(
+                      tag: 'logo$index',
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                            image: AssetImage(_images[index].imagePath),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: _images.length,
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildImageGridView() {
+    var dateToImages = new Map();
+    _images.sort((a, b) => a.date.compareTo(b.date));
+    //group images by date
+    for (int i = 0; i < _images.length; i++) {
+      var d = _images[i].date; //convert string to Datetime
+      if (dateToImages.containsKey(d)) {
+        dateToImages[d].add(_images[i]);
+      } else {
+        dateToImages[d] = [_images[i]];
+        print(dateToImages);
+      }
+    }
+    var sortedKeys = dateToImages.keys.toList()..sort((a, b) => a.compareTo(b));
+    List<Widget> children = [];
+    for (int i = 0; i < sortedKeys.length; i++) {
+      var date = sortedKeys[i];
+      children.add(Text(date,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)));
+      var images = dateToImages[date];
+      Container gridviewContainer = Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return RawMaterialButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AlbumDetails(
+                      imagePath: images[index].imagePath,
+                      date: images[index].date,
+                      mealType: images[index].mealType,
+                      percentage: images[index].percentage,
+                      details: images[index].details,
+                      index: index,
+                    ),
+                  ),
+                );
+              },
+              child: Hero(
+                tag: 'logo$index',
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: AssetImage(images[index].imagePath),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: images.length,
+        ),
+      );
+      children.add(gridviewContainer);
+    }
+    return children;
+  }
+}
+
+class ImageDetails {
+  final String imagePath;
+  final String percentage;
+  final String mealType;
+  final String date;
+  final String details;
+  ImageDetails({
+    required this.imagePath,
+    required this.percentage,
+    required this.mealType,
+    required this.date,
+    required this.details,
+  });
+}
--- osam2021_raspi/src/database.py
+++ osam2021_raspi/src/database.py
@@ -56,7 +56,7 @@ def firebase_send_user_waste(id, waste_list):
     #send to Firebase Firestore Database
     db.collection(u'USER_FOOD_WASTE').document(id).collection(date_meal[3:5]).document(date_meal).set(data)
 
-#Function to send image to Firebase Storage
+#Function to send image to Firebase Storage and send image path to Firebase Firestore
 def firestore_send_image(id, image_address):
     #set image path in Firebase Storage
     blob = bucket.blob(id +'/'+date_meal +'.png')
@@ -66,6 +66,15 @@ def firestore_send_image(id, image_address):
     blob.metadata = metadata
     #upload image to Firebase Storage
     blob.upload_from_filename(image_address)
+    #make blob public for simple access
+    blob.make_public()
+    #get url and set data
+    blob_url = blob.public_url
+    data = {
+        u'IMAGE_ADDRESS': blob_url
+    }
+    #send to Firebase Firestore Database
+    db.collection(u'IMAGES').document(id).collection('WASTE_IMAGES').document(date_meal).set(data)
 
 
 
@@ -76,25 +85,12 @@ if __name__=="__main__":
     #path for raspi
     i_address = '/home/pi/osam/APP_IOT_AI_Meal-Mil-Scan_FOODFIGHTERS/osam2021_raspi/asset/test_image/100_per/100per.png'
     #path for codespace
-    #i_address = "/workspaces/APP_IOT_AI_Meal-Mil-Scan_FOODFIGHTERS/osam2021_raspi/asset/test_image/100_per/100per.png"
-    #Currently not working in raspi
+    #i_address = "/workspaces/APP_IOT_AI_Meal-Mil-Scan_FOODFIGHTERS/Meal_Mil_Scan/assets/images/meal2.jpg"
     firebase_send_meal(b_code)
     firebase_send_user_waste(id,w_list)
     firestore_send_image(id, i_address)
-    print("Successfully send data to Firebase")
-    
+    print("Successfully sent data to Firebase")
 
-
-
-"""
-어차피 firestore써서 필요없을듯 
-#Function to send image address saved in dropbox to Firebase Firestore Database
-def firebase_send_image_address(id, image_address):
-    data = {
-    u'IMAGE_ADDRESS': image_address
-    }
-    db.collection(u'IMAGES').document(id).collection(u'WASTE_IMAGES').document(date_meal).set(data)
-"""
 '''
 App key
 h18mokh27adozq8
