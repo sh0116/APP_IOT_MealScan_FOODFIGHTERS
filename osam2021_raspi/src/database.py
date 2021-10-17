011fc19119d57f6a4526fdddab69808e2f1b99df
sh0116 <38518675+sh0116@users.noreply.github.com>
2021-10-17 05:55:46 +0000

    up

--- osam2021_raspi/src/database.py
+++ osam2021_raspi/src/database.py
@@ -5,9 +5,9 @@ from firebase_admin import firestore
 from firebase_admin import storage
 from uuid import uuid4
 # in codespace
-import today_menu
+#import today_menu
 # in IoT
-#from src import today_menu
+from src import today_menu
 from datetime import date, datetime, timedelta
 import time
 import sys
