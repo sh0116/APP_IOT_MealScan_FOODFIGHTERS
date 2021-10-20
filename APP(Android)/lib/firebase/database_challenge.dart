2bfccd964979c1bc192aecb77385f642f3879310
alee2022 <59150653+alee2022@users.noreply.github.com>
2021-10-20 05:33:42 +0000

    waiting'

--- APP(Android)/lib/firebase/database_challenge.dart
+++ APP(Android)/lib/firebase/database_challenge.dart
@@ -10,16 +10,6 @@ class ChallengeDataBase {
   final CollectionReference collectionRef =
       FirebaseFirestore.instance.collection("CHALLENGES");
 
-<<<<<<< HEAD
-   Future getChallengeData() async {
-    try {
-      // Firebase "CHALLENGES" collection에 있는 모든 챌린지들을 읽어서 리스트에 저장후 리턴함.
-      await collectionRef.get().then((querySnapshot) {
-        for (var result in querySnapshot.docs) {
-          challengesList.add(result.data());
-        }
-      });
-=======
     //챌린지 정보들이 불러와지면 리스트에 정보를 담아 반환
     Future getChallengeData() async {
       try {
@@ -28,7 +18,6 @@ class ChallengeDataBase {
             challengesList.add(result.data());
           }
         });
->>>>>>> 4e5e8f317f599935d181d6366a67990f8af9a2a8
 
         return challengesList;
         //그렇지 않을시 null 값 반환
